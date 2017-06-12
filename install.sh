719645765348cb2893e4803e8d6428daacf5248d
Filip Krumpe <filip.krumpe@fmi.uni-stuttgart.de>
2017-06-12 16:10:26 +0200

    Make pipeline.sh a softlink to the scripts/pipeline.sh script to prevent update failures

--- install.sh
+++ install.sh
@@ -3,12 +3,10 @@
 
 mkdir bin
 
-# cp the scripts to the bin directory
-cp scripts/pipeline.sh bin
-chmod +x bin/pipeline.sh
-
 # change into the bin directory and compile the binaries
 cd bin
+ln -s ../scripts/pipeline.sh pipeline.sh
+chmod +x pipeline.sh
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j4
 
